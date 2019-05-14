c391ab741600c7da5e4a12d212a427fa91d6efcc
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-14 19:45:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
     return false
   else
     (2..number-1).to_a.all? do |possible_factor|
-      number % possible_factor != =
+      number % possible_factor != 0
     end
   end
 end
